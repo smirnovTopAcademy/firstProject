0a00b7ca547c44f605c86412daef9b0c8a240e47
Smirnov AV <smirnov_av@t.top-academy.ru>
2023-06-21 20:34:09 +0300

    init string function

--- public/index.php
+++ public/index.php
@@ -1,4 +1,95 @@
 <?php
 
-echo "Hello world";
+echo '<pre>';
+print_r($_GET);
+echo '</pre>';
+
+
+$var3 = $_GET['var3'] ?? '';
+$var3 = isset($_GET['var3']) ? $_GET['var3'] : '';
+
+echo $var3;
+
+$strlen = $_GET['strlen'] ?? '';
+
+if ($strlen) {
+	echo 'strlen => ' . strlen($strlen) . '<br>';
+}
+
+
+$substr = $_GET['substr'] ?? '';
+
+if ($substr) {
+	echo 'substr => ' . substr($substr, -3) . '<br>';
+}
+
+
+$stripos = $_GET['stripos'] ?? '';
+
+if ($stripos) {
+	echo 'stripos => ' ;
+	var_dump(stripos($stripos, 'a'));
+	echo '<br>';
+}
+
+
+
+$strtolower = $_GET['strtolower'] ?? '';
+
+if ($strtolower) {
+	echo 'strtolower => ' ;
+	echo strtolower($strtolower);
+	echo '<br>';
+}
+
+
+
+$key = $_GET['explode'] ?? '';
+
+if ($key) {
+	echo 'explode => ' ;
+	echo '<pre>';
+	print_r(explode('|', $key));
+	echo '</pre>';
+	echo '<br>';
+}
+
+$countries = [
+	'Russia',
+	'Belorus',
+	'Kazahstan',
+];
+
+$countryList = implode(', ', $countries);
+echo $countryList;
+echo '<br>';
+
+$letter = 'Hello, %name%! Nice to meet you ';
+
+$name = 'John';
+
+echo strtr(
+	$letter, [
+		'%name%' => $name
+	]
+);
+echo '<br>';
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
